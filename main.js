bddfef0ff93b8f0595781b3948ecbbf9988cfb6a
DAX5 <44905983+DAX5@users.noreply.github.com>
2019-03-25 16:23:18 -0300

    1.0.0

--- main.js
+++ main.js
@@ -2,38 +2,30 @@
 	'use strict';
 
 	/*
-	Vamos estruturar um pequeno app utilizando módulos.
-	Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-	A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-	seguinte forma:
-	- No início do arquivo, deverá ter as informações da sua empresa - nome e
-	telefone (já vamos ver como isso vai ser feito)
-	- Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-	um formulário para cadastro do carro, com os seguintes campos:
-		- Imagem do carro (deverá aceitar uma URL)
-		- Marca / Modelo
-		- Ano
-		- Placa
-		- Cor
-		- e um botão "Cadastrar"
-
-	Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-	carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-	aparecer no final da tabela.
-
-	Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-	empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-	Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-	no arquivo company.json que já está criado.
-
-	Essas informações devem ser adicionadas no HTML via Ajax.
-
-	Parte técnica:
-	Separe o nosso módulo de DOM criado nas últimas aulas em
-	um arquivo DOM.js.
-
-	E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-	que será nomeado de "app".
+	Já temos as funcionalidades de adicionar e remover um carro. Agora, vamos persistir esses dados, 
+	salvando-os temporariamente na memória de um servidor.
+	Nesse diretório do `challenge-32` tem uma pasta `server`. É um servidor simples, em NodeJS, para 
+	que possamos utilizar para salvar as informações dos nossos carros.
+	Para utilizá-lo, você vai precisar fazer o seguinte:
+	- Via terminal, acesse o diretório `server`;
+	- execute o comando `npm install` para instalar as dependências;
+	- execute `node app.js` para iniciar o servidor.
+	Ele irá ser executado na porta 3000, que pode ser acessada via browser no endereço: 
+	`http://localhost:3000`
+	O seu projeto não precisa estar rodando junto com o servidor. Ele pode estar em outra porta.
+	As mudanças que você irá precisar fazer no seu projeto são:
+	- Para listar os carros cadastrados ao carregar o seu projeto, faça um request GET no endereço
+	`http://localhost:3000/car`
+	- Para cadastrar um novo carro, faça um POST no endereço `http://localhost:3000/car`, enviando
+	os seguintes campos:
+	  - `image` com a URL da imagem do carro;
+	  - `brandModel`, com a marca e modelo do carro;
+	  - `year`, com o ano do carro;
+	  - `plate`, com a placa do carro;
+	  - `color`, com a cor do carro.
+	Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o novo carro cadastrado.
+	Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
+	do pull request.
 	*/
 
 	var app = (function appController(){
@@ -48,8 +40,6 @@
 			},
 
 			removeCar: function removeCar(id, table){
-				
-
 				Array.prototype.forEach.call(table.childNodes, function(item){
 					if(item.id === id){
 						item.remove();
@@ -116,7 +106,6 @@
 				ajax.open('GET', '/company.json', true);
 				ajax.send();
 				ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
-
 			},
 
 			getCompanyInfo: function getCompanyInfo(){

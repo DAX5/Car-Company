6ba692eb1ba173a03bff4f7659bc6372a9447796
DAX5 <44905983+DAX5@users.noreply.github.com>
2020-03-15 14:44:12 -0300

    1.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Car-Company
 Aluguel de Carros
+
+Rquisitos:
+Python, Node
+
+Windowns:
+- Command Prompt
+ - cd ../Car-Company/
+ - python -m http.server
+
+- Comand Prompt
+ - cd ../Car-Company/server
+ - node app.js
--- main.js
+++ main.js
@@ -1,35 +1,6 @@
 (function($){
 	'use strict';
-
-	/*
-	Hora de finalizar nosso projeto!
-
-	Já temos o cadastro funcionando e persistindo em memória;
-	Já estamos deletando o carro da tabela (no frontend).
-
-	Mas se você perceber, se você recarregar a tela, o carro ainda vai estar lá.
-	Agora você precisa fazer com que, ao clicar no botão de deletar, o carro seja
-	removido da tabela e também seja deletado do servidor.
-
-	Para fazer isso, você precisa enviar o verbo HTTP "DELETE" para a mesma URL
-	que você faz o POST para cadastrar o carro:
-	`http://localhost:3000/car`, só que, ao invés de enviar todas as informações
-	do carro, como você faz para cadastrar, você deve enviar somente a placa
-	do carro.
-
-	Fazendo isso, ao recarregar a tela, a tabela deve mostrar os carros atualizados.
-
-	A lógica do servidor que está criada nesso diretório desse desafio é o mesmo
-	do desafio anterior, com a diferença que, nesse desafio, nós temos a
-	implementação da regra para a deleção do carro =)
-
-	A regra é a mesma das anteriores: crie uma branch `challenge-33` no seu
-	repositório do GitHub, e envie o pull request para lá.
-
-	Depois, envie um pull request no repositório do curso, colocando no console.log
-	abaixo a URL do pull request no seu repositório.
-	*/
-
+	
 	var app = (function appController(){
 		return {
 			init: function init(){
@@ -45,7 +16,13 @@
 			handleSubmit: function handleSubmit(e){
 				e.preventDefault();
 				var car = app.createNewCar();
-				app.postData(car);
+				if(!app.validatesCar(car.plate)){
+					app.postData(car);
+				}
+				else{
+					alert('Veículo já cadastrado');
+					app.clearInputs();
+				}
 			},
 
 			createNewCar: function createNewCar(){
@@ -88,6 +65,19 @@
 				ajax.open('GET', 'http://localhost:3000/car', true);
 				ajax.send();
 				ajax.addEventListener('readystatechange', app.hasCar, false);
+			},
+
+			validatesCar: function validatesCar(plate){
+				var $tableCar = $('[data-js="table-car"]').get();
+				if($tableCar.childElementCount){
+					var $plateExisting = $('tr').element;
+					for(var i=1; i<$plateExisting.length; i++){
+						if($plateExisting[i].cells[3].innerText == plate){
+							return true;
+						}
+					}
+					return false;
+				}
 			},
 
 			hasCar: function hasCar(){
